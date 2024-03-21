23a75efdc35a134d7bd40d6d80671bd4ceb3ba0f
xdhiab <162072082+xdhiab@users.noreply.github.com>
2024-03-21 22:46:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-45a5b457-7f46-41a9-bdc3-6b0dd5c5c111/)
 
 
 echo "$curl_output" >> Update.tsx
